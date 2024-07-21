ef43a50ee8c2ebb1f0472b0e7b5dd0dc34eacf05
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-21 17:12:32 +0200

    Dodan gumb za navigaciju

--- src/app/projekt/components/projekt-lista/projekt-lista.component.ts
+++ src/app/projekt/components/projekt-lista/projekt-lista.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { PrimengModule } from '../../../shared/modules/primeng/primeng.module';
 import { ProjektService } from '../../services/projekt.service';
 import { ProjektCardComponent } from '../projekt-card/projekt-card.component';
+import { Router } from '@angular/router';
+import { ProjektRute } from '../../routes/projekt-rute';
+import { AppRute } from '../../../routes/app-rute';
 
 @Component({
   selector: 'projekt-lista',
@@ -13,5 +16,9 @@ import { ProjektCardComponent } from '../projekt-card/projekt-card.component';
 export class ProjektListaComponent {
   projekti$ = this.projektService.getProjekti();
 
-  constructor(private projektService: ProjektService) {}
+  constructor(private projektService: ProjektService, private ruter: Router) {}
+
+  navigirajNaIzraduProjekta() {
+    this.ruter.navigateByUrl(`/${AppRute.Projekt}/${ProjektRute.Novi}`);
+  }
 }

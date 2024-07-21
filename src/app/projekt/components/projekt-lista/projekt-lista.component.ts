9097903c061eabb23e1aa6fd882fa41df6a0ce08
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-21 16:29:06 +0200

    Generirana komponenta

---
+++ src/app/projekt/components/projekt-lista/projekt-lista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'projekt-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './projekt-lista.component.html',
+  styleUrl: './projekt-lista.component.css',
+})
+export class ProjektListaComponent {}

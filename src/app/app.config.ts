bd9588b02e81cd9c9a39295287be69f6fb10df76
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-21 18:17:29 +0200

    Implementirano izrada projekta

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './routes/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     importProvidersFrom(BrowserAnimationsModule),
+    MessageService,
   ],
 };
 
--- src/app/projekt/components/projekt-novi/projekt-novi.component.ts
+++ src/app/projekt/components/projekt-novi/projekt-novi.component.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
+import { PrimengModule } from '../../../shared/modules/primeng/primeng.module';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { KorisnikService } from '../../../korisnik/services/korisnik.service';
+import { map } from 'rxjs';
+import { ProjektService } from '../../services/projekt.service';
+import { ToastService } from '../../../shared/services/toast.service';
+import { Router } from '@angular/router';
+import { AppRute } from '../../../routes/app-rute';
+import { ProjektRute } from '../../routes/projekt-rute';
 
 @Component({
   selector: 'app-projekt-novi',
   standalone: true,
-  imports: [],
+  imports: [PrimengModule, ReactiveFormsModule],
   templateUrl: './projekt-novi.component.html',
-  styleUrl: './projekt-novi.component.css'
+  styleUrl: './projekt-novi.component.css',
 })
 export class ProjektNoviComponent {
+  forma!: FormGroup;
+  korisnici$ = this.korisnikService.getKorisnici().pipe(
+    map((korisnici) =>
+      korisnici.map((korisnik) => {
+        return {
+          naziv: korisnik.ime + ' ' + korisnik.prezime,
+          id: korisnik.id,
+        };
+      })
+    )
+  );
 
+  constructor(
+    private korisnikService: KorisnikService,
+    private projektService: ProjektService,
+    private toast: ToastService,
+    private ruter: Router
+  ) {}
+
+  ngOnInit() {
+    this.forma = new FormGroup({
+      naziv: new FormControl('', [Validators.required]),
+      voditelji: new FormControl(
+        [],
+        [Validators.required, Validators.minLength(1)]
+      ),
+    });
+  }
+
+  dodajProjekt() {
+    if (this.forma.invalid) {
+      return;
+    }
+
+    this.projektService.createProjekt(this.forma.value).subscribe({
+      next: (r) => {
+        this.toast.showSuccess('Uspješno dodan projekt');
+        this.ruter.navigateByUrl(`/${AppRute.Projekt}/${ProjektRute.Lista}`);
+      },
+      error: (err) => {
+        console.log(err);
+        this.toast.showError(
+          err.error.message || 'Greška prilikom dodavanja projekta'
+        );
+      },
+    });
+  }
 }

1cfd252b7f47c887ad506742237d43ead66500d2
dpiskovic21 <dpiskovic21@foi.hr>
2024-07-30 22:26:44 +0200

    prikaz dostupnih projekta za statistiku

--- src/app/statistika/components/statistika-home/statistika-home.component.ts
+++ src/app/statistika/components/statistika-home/statistika-home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Projekt } from '../../../projekt/interfaces';
 import { ProjektService } from '../../../projekt/services/projekt.service';
 import { AutorizacijaService } from '../../../autorizacija/services/autorizacija.service';
+import { PrimengModule } from '../../../shared/modules/primeng/primeng.module';
 
 @Component({
   selector: 'app-statistika-home',
   standalone: true,
-  imports: [],
+  imports: [PrimengModule],
   templateUrl: './statistika-home.component.html',
   styleUrl: './statistika-home.component.css',
 })

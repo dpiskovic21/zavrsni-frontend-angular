8f641f33f808cee7e4ecc444d285543b9e76eb40
dpiskovic21 <dpiskovic21@foi.hr>
2024-08-05 19:03:39 +0200

    refaktoriran prikaz grafova

--- src/app/statistika/components/statistika-grafovi/statistika-grafovi.component.ts
+++ src/app/statistika/components/statistika-grafovi/statistika-grafovi.component.ts
@@ -3,6 +3,11 @@ import { Projekt } from '../../../projekt/interfaces';
 import { PrimengModule } from '../../../shared/modules/primeng/primeng.module';
 import { ProjektService } from '../../../projekt/services/projekt.service';
 
+interface GrafPodaci {
+  labels: string[];
+  datasets: [{ data: number[]; backgroundColor: string[] }];
+}
+
 @Component({
   selector: 'statistika-grafovi',
   standalone: true,
@@ -11,49 +16,65 @@ import { ProjektService } from '../../../projekt/services/projekt.service';
   styleUrl: './statistika-grafovi.component.css',
 })
 export class StatistikaGrafoviComponent {
-  projekt: Projekt | null = null;
-  data: any = {
-    labels: [],
-    datasets: [{ data: [], backgroundColor: [] }],
-  };
-  getRandomColor() {
-    var color = Math.floor(0x1000000 * Math.random()).toString(16);
-    return '#' + ('000000' + color).slice(-6);
-  }
-  options: any;
-  @Input() set odabraniProjekt(p: Projekt | null) {
-    console.log(p);
-    if (p != null) {
-      this.projekt = p;
-      this.projektServis
-        .getProjektStatistika(this.projekt?.id!)
-        .subscribe((statistika) => {
-          const documentStyle = getComputedStyle(document.documentElement);
-          const textColor = documentStyle.getPropertyValue('--text-color');
-          for (const id in statistika.korisniciSaBrojemZadataka) {
-            this.data.labels.push(
-              statistika.korisniciSaBrojemZadataka[id].naziv
-            );
-            this.data.datasets[0].data.push(
-              statistika.korisniciSaBrojemZadataka[id].brojZadataka
-            );
-            this.data.datasets[0].backgroundColor.push(this.getRandomColor());
-          }
-          this.options = {
-            plugins: {
-              legend: {
-                labels: {
-                  usePointStyle: true,
-                  color: textColor,
-                },
-              },
-            },
-          };
-        });
-    }
-  }
+  brojZadatakaPoKorisniku!: GrafPodaci;
+  zakasnjeliRokovi!: GrafPodaci;
 
   constructor(private projektServis: ProjektService) {}
 
-  ngOnInit() {}
+  @Input() set odabraniProjekt(projekt: Projekt | null) {
+    if (projekt == null) return;
+
+    this.projektServis
+      .getProjektStatistika(projekt.id)
+      .subscribe((statistika) => {
+        this.postaviPodatkeZaBrojZadatakaPoKorisniku(
+          statistika.korisniciSaBrojemZadataka
+        );
+        this.postaviPodatkeZaZakasnjeljeRokove(statistika.zakasnjeliRokovi);
+      });
+  }
+
+  postaviPodatkeZaBrojZadatakaPoKorisniku(korisniciSaBrojemZadataka: {
+    [idKorisnika: string]: { naziv: string; brojZadataka: number };
+  }) {
+    this.brojZadatakaPoKorisniku = {
+      labels: [],
+      datasets: [{ data: [], backgroundColor: [] }],
+    };
+    for (const id in korisniciSaBrojemZadataka) {
+      this.brojZadatakaPoKorisniku.labels.push(
+        korisniciSaBrojemZadataka[id].naziv
+      );
+      this.brojZadatakaPoKorisniku.datasets[0].data.push(
+        korisniciSaBrojemZadataka[id].brojZadataka
+      );
+      this.brojZadatakaPoKorisniku.datasets[0].backgroundColor.push(
+        this.getNasumicnaBoja()
+      );
+    }
+  }
+
+  postaviPodatkeZaZakasnjeljeRokove(zakasnjeliRokovi: {
+    [idKorisnika: string]: { naziv: string; brojZadataka: number };
+  }) {
+    this.zakasnjeliRokovi = {
+      labels: [],
+      datasets: [{ data: [], backgroundColor: [] }],
+    };
+
+    for (const id in zakasnjeliRokovi) {
+      this.zakasnjeliRokovi.labels.push(zakasnjeliRokovi[id].naziv);
+      this.zakasnjeliRokovi.datasets[0].data.push(
+        zakasnjeliRokovi[id].brojZadataka
+      );
+      this.zakasnjeliRokovi.datasets[0].backgroundColor.push(
+        this.getNasumicnaBoja()
+      );
+    }
+  }
+
+  getNasumicnaBoja() {
+    var color = Math.floor(0x1000000 * Math.random()).toString(16);
+    return '#' + color;
+  }
 }

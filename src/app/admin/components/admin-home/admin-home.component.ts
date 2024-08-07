bdd1767069e8c738aaace44cfdb1208d026b08fa
dpiskovic21 <dpiskovic21@foi.hr>
2024-08-07 13:05:35 +0200

    css

--- src/app/admin/components/admin-home/admin-home.component.ts
+++ src/app/admin/components/admin-home/admin-home.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { KorisnikService } from '../../../korisnik/services/korisnik.service';
-import { Korisnik } from '../../../korisnik/interfaces';
 import { PrimengModule } from '../../../shared/modules/primeng/primeng.module';
-import { Column } from '../../../shared/interfaces';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AdminRute } from '../../routes/admin-rute';
 
 @Component({
   selector: 'admin-home',
   standalone: true,
-  imports: [PrimengModule, RouterLink, RouterOutlet],
+  imports: [PrimengModule, RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './admin-home.component.html',
   styleUrl: './admin-home.component.css',
 })
